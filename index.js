64aeaaccbe60e3401d9abca46a0903fd8397c2b3
Unknownempire <pranav.taru.21@gmail.com>
2023-05-21 02:49:05 +0530

    main js file

---
+++ index.js
@@ -0,0 +1,44 @@
+var myarray =[ 
+    {
+        "id": 1,
+        "title": "Two sum",
+        "difficulty": "Easy",
+        "acceptance": '56%',
+        "status": "AC"
+    },
+    {
+        "id": 2,
+        "title": "Three Sum",
+        "difficulty": "Medium",
+        "acceptance": '23%',
+        "status": "AC"
+    },
+    {
+        "id": 3,
+        "title": "Binary Search",
+        "difficulty": "Easy",
+        "acceptance": '89%',
+        "status": "AC",
+    },
+    {
+        "id": 4,
+        "title": "Swap two nodes in tree",
+        "difficulty": "Easy",
+        "acceptance": '78%',
+        "status": "WA"
+    },
+    {
+        "id": 5,
+        "title": "Find Peak of a sorted list",
+        "difficulty": "Medium",
+        "acceptance": '45%',
+        "status": "AC"
+    },
+    {
+        "id": 6,
+        "title": "DFS",
+        "difficulty": "Medium",
+        "acceptance": '53%',
+        "status": "AC",
+    }
+]
